3e0b723fb06594f2e15a33378cbc86b4fb6197c4
TheFull0 <joester17@gmail.com>
2025-01-24 00:50:32 +0200

    Game Manager
    layout of basic concepts that are needed for later
    game level controller
    events for starting and ending rhythm

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,72 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.EventSystems;
+using System;
 
 public class GameManager : MonoBehaviour
 {
+    //class references
     [SerializeField] private PlayerController playerController;
-    [SerializeField] private GameObject[] rhythms;
+    [SerializeField] private UiManager uiManager;
+    
+    //player progression track
+    private GameState _gameState;
+    
+    //rhythm variables
+    [SerializeField] private GameObject[] _rhythms;
+    private int _rhythmsIndex = 0;
+    [SerializeField]private float _maxTimer;
+    private float _timer;
+    
+    //events 
+    private event Action OnRhythmSectionStart;
+    private event Action OnRhythmSectionEnd;
+
+    void OnEnable()
+    {
+        //event subscriptions
+    }
+    
+    void start()
+    {
+        _gameState = GameState.LevelOne;
+    }
+
+    void OnDisable()
+    {
+        //event unsubscription
+    }
+
+    private void PlayerPassedRhythm()
+    {
+        _gameState++;
+        if (_gameState == GameState.LevelMax)
+        {
+            //invoke end of game
+        }
+        else
+        {
+            //invoke next level 
+        }
+    }
+    
+
+    public void InvokeOnRhythmSectionStart()
+    {
+        OnRhythmSectionStart?.Invoke();
+    }
+    public void InvokeOnRhythmSectionEnd()
+    {
+        OnRhythmSectionEnd?.Invoke();
+    }
+
+    private enum GameState
+    {
+        LevelOne,
+        LevelTwo,
+        LevelThree,
+        LevelFour,
+        LevelMax,
+    }
 }
